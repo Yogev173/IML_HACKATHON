e2a963e6a1af0cf4bf74ba82e93f97dd5be38b6c
TLP-288 <omer.mor-yosef@mail.huji.ac.il>
2023-06-07 23:07:34 +0300

    save my shit

--- main1.py
+++ main1.py
@@ -3,86 +3,35 @@ import numpy as np
 import pandas as pd
 pio.templates.default = "simple_white"
 
+# stages - LA is level 3 - locally advanced, it is in range 3a 3b 3c, so we will make it average - 3b
+# meaning every time we have
+FEATURED_TO_DROP = ['id-hushed_internalpatientid', 'Form Name', ]
 
-def load_data(filename: str):
-    """
-    Load house prices dataset and preprocess data.
-    Parameters
-    ----------
-    filename: str
-        Path to house prices dataset
+all_features = [' Hospital', 'User Name', 'אבחנה-Age', 'אבחנה-Basic stage',
+       'אבחנה-Diagnosis date', 'אבחנה-Her2', 'אבחנה-Histological diagnosis',
+       'אבחנה-Histopatological degree', 'אבחנה-Ivi -Lymphovascular invasion',
+       'אבחנה-KI67 protein', 'אבחנה-Lymphatic penetration',
+       'אבחנה-M -metastases mark (TNM)', 'אבחנה-Margin Type',
+       'אבחנה-N -lymph nodes mark (TNM)', 'אבחנה-Nodes exam',
+       'אבחנה-Positive nodes', 'אבחנה-Side', 'אבחנה-Stage',
+       'אבחנה-Surgery date1', 'אבחנה-Surgery date2', 'אבחנה-Surgery date3',
+       'אבחנה-Surgery name1', 'אבחנה-Surgery name2', 'אבחנה-Surgery name3',
+       'אבחנה-Surgery sum', 'אבחנה-T -Tumor mark (TNM)', 'אבחנה-Tumor depth',
+       'אבחנה-Tumor width', 'אבחנה-er', 'אבחנה-pr',
+       'surgery before or after-Activity date',
+       'surgery before or after-Actual activity',
+       'id-hushed_internalpatientid']
+print(len(all_features))
 
-    Returns
-    -------
-    Design matrix and response vector (prices) - either as a single
-    DataFrame or a Tuple[DataFrame, Series]
+def load_data(filename: str) -> pd.DataFrame:
     """
-    df = pd.read_csv(filename).dropna().drop_duplicates()
+    load data from a csv file filename.csv into a pandas dataframe
+    """
+    # TODO DROPNA?
+    df = pd.read_csv(filename).drop_duplicates()
+    df = df.drop(FEATURED_TO_DROP, axis=1)
+    # mission 1: drop the not important ones and add other important ones
+
+    # mission 2: preprocess the data to be able to use it in the model,
+    # and use it wisely todo add descrioption of specific activities!
     return df
-
-def split_train_test(X: pd.DataFrame, y: pd.Series, train_proportion: float = .75) \
-        -> Tuple[pd.DataFrame, pd.Series, pd.DataFrame, pd.Series]:
-    """
-    Randomly split given sample to a training- and testing sample
-
-    Parameters
-    ----------
-    X : DataFrame of shape (n_samples, n_features)
-        Data frame of samples and feature values.
-
-    y : Series of shape (n_samples, )
-        Responses corresponding samples in data frame.
-
-    train_proportion: Fraction of samples to be split as training set
-
-    Returns
-    -------
-    train_X : DataFrame of shape (ceil(train_proportion * n_samples), n_features)
-        Design matrix of train set
-
-    train_y : Series of shape (ceil(train_proportion * n_samples), )
-        Responses of training samples
-
-    test_X : DataFrame of shape (floor((1-train_proportion) * n_samples), n_features)
-        Design matrix of test set
-
-    test_y : Series of shape (floor((1-train_proportion) * n_samples), )
-        Responses of test samples
-
-    """
-    train = X.sample(frac=train_proportion)
-    test = X.loc[X.index.difference(train.index)]
-    return train, y.loc[train.index], test, y.loc[test.index]
-
-if __name__ == '__main__':
-    np.random.seed(0)
-    # Load and preprocessing the dataset
-    df, price = load_data("../datasets/house_prices.csv")
-
-    # Split samples into training- and testing sets.
-    train_X, train_y, test_X, test_y = split_train_test(df, price)
-
-    # Question 4 - Fit model over increasing percentages of the overall training data
-    # For every percentage p in 10%, 11%, ..., 100%, repeat the following 10 times:
-    #   1) Sample p% of the overall training data
-    #   2) Fit linear model (including intercept) over sampled set
-    #   3) Test fitted model over test set
-    #   4) Store average and variance of loss over test set
-    # Then plot average loss as function of training size with error ribbon of size (mean-2*std, mean+2*std)
-    ps = list(range(10, 101))
-    results = np.zeros((len(ps), 10))
-    for i, p in enumerate(ps):
-        for j in range(results.shape[1]):
-            _X = train_X.sample(frac=p / 100.0)
-            _y = train_y.loc[_X.index]
-            results[i, j] = LinearRegression(include_intercept=True).fit(_X, _y).loss(test_X, test_y)
-
-    m, s = results.mean(axis=1), results.std(axis=1)
-    fig = go.Figure([go.Scatter(x=ps, y=m-2*s, fill=None, mode="lines", line=dict(color="lightgrey")),
-                     go.Scatter(x=ps, y=m+2*s, fill='tonexty', mode="lines", line=dict(color="lightgrey")),
-                     go.Scatter(x=ps, y=m, mode="markers+lines", marker=dict(color="black"))],
-                    layout=go.Layout(title="Test MSE as Function Of Training Size",
-                                     xaxis=dict(title="Percentage of Training Set"),
-                                     yaxis=dict(title="MSE Over Test Set"),
-                                     showlegend=False))
-    fig.write_image("mse.over.training.percentage.png")
